2eab6a8a7154e5cafb75d36d29cda608996fd346
unknown <g00302726@atu.ie>
2022-12-16 11:40:02 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,7 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-const cors = require('cors');
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
+/*const cors = require('cors');
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,7 +21,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 
 //mongodb+srv://admin:<password>@cluster0.8taek.mongodb.net/?retryWrites=true&w=majority
 // getting-started.js
@@ -76,6 +80,13 @@ app.delete('/api/book/:id',(req, res)=>{
     res.send(data);
   })
 })
+
+//add at the bottom just over app.listen
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../build/index.html'));
+  });
+  
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
